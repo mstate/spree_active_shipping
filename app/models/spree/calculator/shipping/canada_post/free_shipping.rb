8aed4fdd27151dc1662b1001f6e717e93751ad2a
mike <mstate@gmail.com>
2014-04-02 21:41:54 -0400

    Add zip code exclusion regular expression
    Configure zip code exclusions by regular expression.

--- app/models/spree/calculator/shipping/canada_post/free_shipping.rb
+++ app/models/spree/calculator/shipping/canada_post/free_shipping.rb
@@ -1,9 +1,10 @@
 module Spree
   module Calculator::Shipping
     module CanadaPost
-      class FreeShipping < Spree::Calculator::Shipping::CanadaPost::Base
+      class FreeShipping #< Spree::Calculator::Shipping::CanadaPost::Base
         preference :minimal_amount, :decimal, default: 100
-        
+        preference :exclude_zip_containing, :decimal, default: 100
+
         def self.description
           I18n.t("canada_post.free_shipping")
         end
@@ -12,11 +13,15 @@ module Spree
           "DOM.EP"
         end
 
+        def available?(package)
+          !compute(package).nil?
+        rescue Spree::ShippingError
+          false
+        end
         def compute_package(package)
           order = package.order
           destination = build_location(order.ship_address)
-          return nil unless destination.country == 'CA'
-          return nil if !(destination.zip =~ /0/)
+          return nil if !Spree::ActiveShipping::Config[:exclude_postal_codes_containing.blank? && !(destination.zip =~ /#{Spree::ActiveShipping::Config[:exclude_postal_codes_containing]}/)
           return nil if order.total >= Spree::ActiveShipping::Config[:minimum_value_for_free_shipping].to_f
           return 0.0
         end
