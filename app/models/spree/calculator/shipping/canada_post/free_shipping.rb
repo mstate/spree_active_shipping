7fcac6bb76e14bf316c848cf3a2d85a300778d38
mike <mstate@gmail.com>
2014-04-02 22:17:49 -0400

    Change display order of preferences

--- app/models/spree/calculator/shipping/canada_post/free_shipping.rb
+++ app/models/spree/calculator/shipping/canada_post/free_shipping.rb
@@ -2,8 +2,8 @@ module Spree
   module Calculator::Shipping
     module CanadaPost
       class FreeShipping < Spree::Calculator::Shipping::CanadaPost::Base
-        preference :exclude_zip_containing, :string
         preference :minimal_amount, :decimal, :default => 100
+        preference :exclude_zip_containing, :string
 
         def self.description
           I18n.t("canada_post.free_shipping")
