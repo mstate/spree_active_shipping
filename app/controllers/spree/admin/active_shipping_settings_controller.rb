059dbacaeb19dd24e34f9ba90ca06fa1464da8ee
mike <mstate@gmail.com>
2014-07-04 23:03:47 -0400

    Added Canada Post Handling Fee to Settings View

--- app/controllers/spree/admin/active_shipping_settings_controller.rb
+++ app/controllers/spree/admin/active_shipping_settings_controller.rb
@@ -4,7 +4,7 @@ class Spree::Admin::ActiveShippingSettingsController < Spree::Admin::BaseControl
     @preferences_UPS = [:ups_login, :ups_password, :ups_key, :shipper_number]
     @preferences_FedEx = [:fedex_login, :fedex_password, :fedex_account, :fedex_key]
     @preferences_USPS = [:usps_login]
-    @preferences_CanadaPost = [:canada_post_key, :canada_post_key_password, :canada_post_customer_number, :canada_post_contract_number]
+    @preferences_CanadaPost = [:canada_post_key, :canada_post_key_password, :canada_post_customer_number, :canada_post_contract_number, :canada_post_handling_fee_cents]
     @preferences_OriginAddress = [:origin_sender, :origin_address1, :origin_address2, :origin_city, :origin_zip, :origin_state, :origin_country ]
     @preferences_GeneralSettings = [:units, :unit_multiplier, :default_weight, :handling_fee, 
       :max_weight_per_package, :test_mode]
